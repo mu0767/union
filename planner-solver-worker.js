48f1f3fac592da942d4bcfe2d68ba7ae2037e8f3
mu0767 <133477271+mu0767@users.noreply.github.com>
2026-09-07 15:41:32 +0900

    Remove OR-Tools dependency from fast planner worker

--- planner-solver-worker.js
+++ planner-solver-worker.js
@@ -1,8 +1,7 @@
-import * as cp from './vendor/ortools/browser/cp-sat.js';
 import {solveRaid} from './planner-solver.js';
 self.onmessage = async event => {
   try {
-    const plan=await solveRaid(event.data,cp,message=>self.postMessage({progress:message}));
+    const plan=await solveRaid(event.data,message=>self.postMessage({progress:message}));
     self.postMessage({plan});
   } catch(error) { self.postMessage({error:error.message || String(error)}); }
 };
