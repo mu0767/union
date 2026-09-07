8fd87d84e3830524f6ad44201f4ca5a6c972d0db
mu0767 <133477271+mu0767@users.noreply.github.com>
2026-09-07 22:18:06 +0900

    Remove retired planner solver worker

--- planner-solver-worker.js
+++
@@ -1,7 +0,0 @@
-import {solveRaid} from './planner-solver.js';
-self.onmessage = async event => {
-  try {
-    const plan=await solveRaid(event.data,message=>self.postMessage({progress:message}));
-    self.postMessage({plan});
-  } catch(error) { self.postMessage({error:error.message || String(error)}); }
-};
